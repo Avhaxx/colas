a3ad115e36ecc7e0f70cbc7f8a37e10930e34b2d
Fmadriz19 <madriz20102001@gmail.com>
2024-01-10 13:11:42 -0400

    feat: he agregado comentarios y eliminado print

--- redes.py
+++ redes.py
@@ -1,6 +1,8 @@
 import simpy
 import random
 
+# Clase clientes
+
 class Cliente:
     def __init__(self, env, nodo, mu, nombre):
         self.env = env
@@ -12,22 +14,20 @@ class Cliente:
         self.action = env.process(self.llegada())
 
     def llegada(self):
-        #print(f"Llega el cliente {self.nombre} al nodo {self.nodo.name} en el tiempo {round(self.env.now, 4)}")
         with self.nodo.resource.request() as req:
             yield req
             self.inicio_atencion_time = round (self.env.now, 4)
-            #print(f"El cliente {self.nombre} comienza a ser atendido en el {self.nodo.name} en el tiempo {round(self.env.now, 4)}")
             yield self.env.timeout(random.expovariate(self.mu))
             self.fin_atencion_time = round(self.env.now, 4)
-            #print(f"El cliente ha sido atendido y deja el {self.nodo.name} en el tiempo {round(self.env.now, 4)}")
-            #print(f" Tiempo de inicio de atención: {self.inicio_atencion_time}, Tiempo de fin de atención: {self.fin_atencion_time}\n")
-        
+    
     def get_fin(self):
         return {
             'Nombre': self.nombre, 
             'Inicio atencion': self.inicio_atencion_time, 
             'Fin atencion': self.fin_atencion_time
         }
+
+# Clase Nodo
 
 class Nodo:
     def __init__(self, env, nombre, capacidad):
